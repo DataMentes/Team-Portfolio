9b337a689cf3f67a7f2a2a9747ac7c747d02c01e
ezz-aldin <ezzaldinaref@gmail.com>
2025-05-12 14:18:55 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+# ---------------------------
+# Import Libraries & Packages
+# ---------------------------
+import streamlit as st
+
+# ---------------------------
+# Team Data Info
+# ---------------------------
+teammates = [
+    {"name": "Ezz Al-Din Emad Ali Aref",
+     "role": "ML Engineer",
+     "bio": "Experienced in machine learning, deep learning, and MLOps.",
+     "github": 'https://github.com/EzzAl-Din',
+     "linkedin": 'https://www.linkedin.com/in/ezzal-din',
+     "portfolio": "https://ezzaldinaref.streamlit.app"
+     },
+    {"name": "Sarah Ali",
+     "role": "Data Analyst",
+     "bio": "Skilled in data visualization and statistical analysis.",
+     "github": 'https://github.com/',
+     "linkedin": 'https://www.linkedin.com/',
+     "portfolio": "https://ezzaldinaref.streamlit.app"
+     },
+    {"name": "Omar El-Sayed",
+     "role": "ML Engineer",
+     "bio": "Passionate about AI and deep learning.",
+     "github": 'https://github.com/',
+     "linkedin": 'https://www.linkedin.com/',
+     "portfolio": "https://ezzaldinaref.streamlit.app"
+     },
+    {"name": "Mona Ibrahim",
+     "role": "Business Analyst",
+     "bio": "Expert in market research and business strategy.",
+     "github": 'https://github.com/',
+     "linkedin": 'https://www.linkedin.com/',
+     "portfolio": "https://ezzaldinaref.streamlit.app"
+     }
+]
+
+
+# ---------------------------
+# Streamlit App
+# ---------------------------
+def main():
+    # ---------------------------
+    # Page Content
+    # ---------------------------
+    st.set_page_config(page_title="Meet the DataMentes Team")
+    st.title("Meet the DataMentes Team")
+    for member in teammates:
+        st.subheader(member["name"])
+        st.text(f"Role: {member["role"]}")
+        st.markdown(f"Summary: {member['bio']}")
+        st.markdown(f"View GitHub: [{member['github']}]({member['github']})")
+        st.markdown(f"View LinkedIn: [{member['linkedin']}]({member['linkedin']})")
+        st.markdown(f"View Portfolio: [{member['portfolio']}]({member['portfolio']})")
+        st.markdown("---")
+
+    # ---------------------------
+    # Footer or Credits
+    # ---------------------------
+    st.markdown("Created by DataMentes Team ")
+
+
+if __name__ == "__main__":
+    main()
